82dbf7559f993e2c04d9174a8f3046685ddf1a62
iustin <iustindeaconu@gmail.com>
2024-08-16 21:47:39 +0300

    workflow-fixes

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -11,12 +11,11 @@ def initiate():
     ]
 
     car_make_instances = []
-    for data in car_make_data: 
+    for data in car_make_data:
         car_make_instances.append(CarMake.objects.create(
-            name=data['name'], 
+            name=data['name'],
             description=data['description']
             ))
-
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
@@ -52,7 +51,7 @@ def initiate():
        "car_make": car_make_instances[4]},
     ]
 
-    for data in car_model_data: 
+    for data in car_model_data:
         CarModel.objects.create(
             name=data['name'],
             car_make=data['car_make'],
