9bbce16c27513e480397f629f8be67c328108f06
iustin <iustindeaconu@gmail.com>
2024-08-17 15:12:25 +0300

    final-step

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -3,12 +3,21 @@ from .models import CarMake, CarModel
 
 
 # Register your models here.
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+
 # CarModelInline class
-
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['car_make', 'name', 'dealer_id', 'model_type', 'year']
+    list_filter = ['model_type', 'car_make', 'dealer_id', 'year',]
+    search_fields = ['car_make', 'name']
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+    inlines = [CarModelInline]
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
